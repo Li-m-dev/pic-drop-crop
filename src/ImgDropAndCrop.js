62964323520844f7f4a800f8d960058cb090dd68
Li Swafford <liswafford@yahoo.com>
2019-04-03 15:28:39 -0500

    verify file

--- src/ImgDropAndCrop.js
+++ src/ImgDropAndCrop.js
@@ -1,35 +1,78 @@
 import React, { Component } from "react";
 import Dropzone from "react-dropzone";
 
-const imageMaxSize = 100;
+const imageMaxSize = 100000000000;
+const acceptedFiletypes =
+  "image/x-png, image/png, image/jpg, image/jpeg, image/gif";
+const acceptedFileTypesArray = acceptedFiletypes
+  .split(",")
+  .map(item => item.trim());
+
 class ImgDropAndCrop extends Component {
+  state = {
+    imgSrc: null
+  };
+
+  verifyFile = files => {
+    if (files && files.length > 0) {
+      const currentFile = files[0];
+      const currentFileType = currentFile.type;
+      const currentFileSize = currentFile.size;
+      if (currentFileSize > imageMaxSize) {
+        alert(
+          "this file is not allowed, " + currentFileSize + " bytes is too large"
+        );
+        return false;
+      }
+      if (!acceptedFileTypesArray.includes(currentFileType)) {
+        alert("this file is not allowed");
+        return false;
+      }
+      return true;
+    }
+  };
   handleOnDrop = (files, rejectedFiles) => {
     console.log(files);
     console.log("rejected files are", rejectedFiles);
     if (rejectedFiles && rejectedFiles.length > 0) {
-      const currentRejectFile = rejectedFiles[0];
-      const currentRejectFileType = currentRejectFile.type;
-      const currentRejectFileSize = currentRejectFile.size;
-      if (currentRejectFileSize > imageMaxSize) {
-        alert("this file is too big");
-      }
+      this.verifyFile(rejectedFiles);
     }
     if (files && files.length > 0) {
-      const currentFile = rejectedFiles[0];
-      const currentFileType = currentFile.type;
-      const currentFileSize = currentFile.size;
-      if (currentFileSize > imageMaxSize) {
-        alert("this file is too big");
+      const isVerified = this.verifyFile(files);
+      if (isVerified) {
+        //imageBase64Data
+        const currentFile = files[0];
+        const myFileReader = new FileReader();
+        myFileReader.addEventListener(
+          "load",
+          () => {
+            console.log(myFileReader.result);
+            this.setState({
+              imgSrc: myFileReader.result
+            });
+          },
+          false
+        );
+        myFileReader.readAsDataURL(currentFile);
       }
     }
   };
   render() {
+    const { imgSrc } = this.state;
     return (
       <div>
         <h1>Drop and Crop</h1>
+        {imgSrc !== null ? (
+          <div>
+            {imgSrc}
+            <img src={imgSrc} />
+          </div>
+        ) : (
+          ""
+        )}
         <Dropzone
           onDrop={this.handleOnDrop}
-          accept="image/*"
+          accept={acceptedFiletypes}
           multiple={false}
           maxSize={imageMaxSize}
         >
