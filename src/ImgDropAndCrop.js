2b7db92b57f5ca23467eb2cbdf3d75f205b92daf
Li Swafford <liswafford@yahoo.com>
2019-04-03 17:00:16 -0500

    add crop and download

--- src/ImgDropAndCrop.js
+++ src/ImgDropAndCrop.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
 import Dropzone from "react-dropzone";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+
+import {
+  base64StringtoFile,
+  downloadBase64File,
+  extractImageFileExtensionFromBase64,
+  image64toCanvasRef
+} from "./ResuableUtils";
 
 const imageMaxSize = 100000000000;
 const acceptedFiletypes =
@@ -9,9 +18,16 @@ const acceptedFileTypesArray = acceptedFiletypes
   .map(item => item.trim());
 
 class ImgDropAndCrop extends Component {
-  state = {
-    imgSrc: null
-  };
+  constructor(props) {
+    super(props);
+    this.imagePreviewCanvasRef = React.createRef();
+    this.state = {
+      imgSrc: null,
+      crop: {
+        aspect: 1 / 1
+      }
+    };
+  }
 
   verifyFile = files => {
     if (files && files.length > 0) {
@@ -32,8 +48,8 @@ class ImgDropAndCrop extends Component {
     }
   };
   handleOnDrop = (files, rejectedFiles) => {
-    console.log(files);
-    console.log("rejected files are", rejectedFiles);
+    // console.log(files);
+    // console.log("rejected files are", rejectedFiles);
     if (rejectedFiles && rejectedFiles.length > 0) {
       this.verifyFile(rejectedFiles);
     }
@@ -46,7 +62,7 @@ class ImgDropAndCrop extends Component {
         myFileReader.addEventListener(
           "load",
           () => {
-            console.log(myFileReader.result);
+            // console.log(myFileReader.result);
             this.setState({
               imgSrc: myFileReader.result
             });
@@ -57,6 +73,39 @@ class ImgDropAndCrop extends Component {
       }
     }
   };
+
+  handleImageLoaded = image => {
+    // console.log(image);
+  };
+  handleOnCropComplete = (crop, pixelCrop) => {
+    // console.log("crop, pixelCrop: ", crop, pixelCrop);
+    const canvasRef = this.imagePreviewCanvasRef.current;
+    // console.log("this.imagePreviewCanvasRef: ", this.imagePreviewCanvasRef);
+    const { imgSrc } = this.state;
+    image64toCanvasRef(canvasRef, imgSrc, pixelCrop);
+  };
+  handleOnCropChange = crop => {
+    console.log(crop);
+    this.setState({ crop });
+  };
+
+  handleDownloadClick = e => {
+    e.preventDefault();
+    const canvasRef = this.imagePreviewCanvasRef.current;
+    // console.log("this.imagePreviewCanvasRef: ", this.imagePreviewCanvasRef);
+    const { imgSrc } = this.state;
+    const fileExtension = extractImageFileExtensionFromBase64(imgSrc);
+    const imgSrcNew = canvasRef.toDataURL("image/" + fileExtension);
+    const filename = "previewFile." + fileExtension;
+
+    //file to be uploaded
+    const myNewCroppedFile = base64StringtoFile(imgSrcNew, filename);
+    console.log("myNewCroppedFile: ", myNewCroppedFile);
+
+    //download file
+    downloadBase64File(imgSrcNew, filename);
+  };
+
   render() {
     const { imgSrc } = this.state;
     return (
@@ -64,27 +113,38 @@ class ImgDropAndCrop extends Component {
         <h1>Drop and Crop</h1>
         {imgSrc !== null ? (
           <div>
-            {imgSrc}
-            <img src={imgSrc} />
+            {/* {imgSrc}
+            <img src={imgSrc} alt="previewImg" /> */}
+            <ReactCrop
+              src={imgSrc}
+              crop={this.state.crop}
+              onImageLoaded={this.handleImageLoaded}
+              onComplete={this.handleOnCropComplete}
+              onChange={this.handleOnCropChange}
+            />
+
+            <br />
+            <p> Preview Canvas Crop</p>
+            <canvas ref={this.imagePreviewCanvasRef} />
+            <button onClick={this.handleDownloadClick}> Download</button>
           </div>
         ) : (
-          ""
+          <Dropzone
+            onDrop={this.handleOnDrop}
+            accept={acceptedFiletypes}
+            multiple={false}
+            maxSize={imageMaxSize}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
         )}
-        <Dropzone
-          onDrop={this.handleOnDrop}
-          accept={acceptedFiletypes}
-          multiple={false}
-          maxSize={imageMaxSize}
-        >
-          {({ getRootProps, getInputProps }) => (
-            <section>
-              <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-              </div>
-            </section>
-          )}
-        </Dropzone>
       </div>
     );
   }
